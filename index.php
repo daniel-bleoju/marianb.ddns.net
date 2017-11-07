5abfca82eac47f1f8f61f25b7b138fb2961c8fed
Marian Bogdan <bogdan.m@hotmail.com>
2017-11-07 10:24:50 +0200

    adaugat form de test

--- index.php
+++ index.php
@@ -6,10 +6,34 @@
 echo "<h1> EVIL LAUGHTER!!! </h1>";
 echo "<br>";
 
-$sum= 3+4;
+$number1 = $_POST['number1'];
+$number2 = $_POST['number2'];
 
-echo "Cica data aduni 3 cu 4 iti da: " . $sum;
+$sum = $number1 + $number2;
 
+if (isset('submit')) {
+
+	echo "Totalul este: " . $sum;
+}
 
 
 ?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tabel</title>
+</head>
+<body>
+      
+<form action ="index.php" method ="post">
+    
+    <input type = "number" name = "number1" placeholder = "Enter Number1"><br>
+    <input type = "number" name = "number2" placeholder = "Enter Number2"> <br> 
+    <input type = "submit" name = "submit">
+    
+</form>
+              
+       
+</body>
+</html>
