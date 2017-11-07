81f6066c2b7722f1d6422276d1e6966404b28b6c
Marian Bogdan <bogdan.m@hotmail.com>
2017-11-07 19:05:27 +0200

    style baiat

--- index.php
+++ index.php
@@ -20,18 +20,24 @@ $sum=$number1+$number2;
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Tabel</title>
+    <title>Testuletz</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 <body>
-      
-<form action="index.php" method="post">
+ <div class="container">  
+  <div class="col-sm-6">    
+
+  <form action="index.php" method="post">
+    <div class="form-group">
+    <input type="number" name="number1" class="form-control" placeholder="Enter Number1"><br>
+    <input type="number" name="number2" class="form-control" placeholder="Enter Number2"><br> 
+    </div>
+    <input class="btn btn-primary" type="submit" name="submit" value="Calculeaza">
+
     
-    <input type="number" name="number1" placeholder="Enter Number1"><br>
-    <input type="number" name="number2" placeholder="Enter Number2"><br> 
-    <input type="submit" name="submit">
-    
-</form>
-              
+  </form>
+ </div>
+ </div>             
        
 </body>
 </html>
